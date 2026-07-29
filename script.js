fef2ec1dbd2dfc3b6019212b3c5f21fdab38fae5
prethwin65-oss <prethwin65@gmail.com>
2026-07-29 17:29:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+async function sendMessage() {
+
+    let input = document.getElementById("message");
+    let message = input.value.trim();
+
+    if (message === "") return;
+
+    let chat = document.getElementById("chat");
+
+    chat.innerHTML += `
+        <p><b>You:</b> ${message}</p>
+    `;
+
+    input.value = "";
+
+    try {
+
+        let response = await fetch("http://127.0.0.1:5000/chat", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                message: message
+            })
+
+        });
+
+        let data = await response.json();
+
+        chat.innerHTML += `
+            <p><b>Spidy-AI:</b> ${data.reply}</p>
+        `;
+
+    } catch (error) {
+
+        chat.innerHTML += `
+            <p><b>Spidy-AI:</b> Unable to connect to backend.</p>
+        `;
+
+    }
+
+    chat.scrollTop = chat.scrollHeight;
+}
